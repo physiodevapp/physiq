fbf9f39ac1d51d6d89f8dbaf3b48d51dd9f8fcc0
physiq-ci <ci@physiq>
2026-07-08 19:25:32 +0000

    deploy: update kinematics satellite

--- kinematics/_next/static/chunks/3a0pgxgn72215.js
+++ kinematics/_next/static/chunks/3a0pgxgn72215.js
@@ -1,7 +1,7 @@
 __turbopack_load_page_chunks__("/", [
-  "static/chunks/1yr-a33cd8gtl.js",
+  "static/chunks/016wz3ezw-dw9.js",
   "static/chunks/1d8tbncs4eqyd.js",
   "static/chunks/3gzswoimhr2pj.js",
   "static/chunks/0ptyaa6c3-9e7.js",
-  "static/chunks/turbopack-3hqqtgxsuynv3.js"
+  "static/chunks/turbopack-22rx-2y4tume_.js"
 ])
--- kinematics/_next/static/eWghMIvzetUrV5pvCezQp/_buildManifest.js
+++ kinematics/_next/static/eWghMIvzetUrV5pvCezQp/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/1f1bejgmxh3-d.js"
+    "static/chunks/3a0pgxgn72215.js"
   ],
   "/_error": [
     "static/chunks/3u1hhtdqvrhjc.js"
